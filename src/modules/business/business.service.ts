0d82701e4fd3a218702bad999b38764574f130b2
Amit Shalev <amits@rapyd.ac>
2022-02-03 17:54:41 +0200

    add get business route

--- src/modules/business/business.service.ts
+++ src/modules/business/business.service.ts
@@ -1,41 +1,43 @@
-// /* eslint-disable class-methods-use-this */
-// import { ICreateBusinessDto } from "./business.interface.js";
-// import businessRepository from "./business.repository.js";
+/* eslint-disable class-methods-use-this */
+import { ICreateBusinessDto } from "./business.interface.js";
+import businessRepository from "./business.repository.js";
 
-// class BusinessService {
-//     // async createBusinessAccount(businessDto: ICreateBusinessDto) {
-//     //     const business = await businessRepository.createBusinessAccount(
-//     //         businessDto
-//     //     );
-//     //     return business;
-//     // }
-//     // async transferBusinessToBusiness(
-//     //     sourceId: string,
-//     //     destinationId: string,
-//     //     amount: number
-//     // ) {
-//     //     const transaction = await businessRepository.transferBusinessToBusiness(
-//     //         sourceId,
-//     //         destinationId,
-//     //         amount
-//     //     );
-//     //     return transaction;
-//     // }
-//     // async transferBusinessToIndividual(
-//     //     sourceId: string,
-//     //     destinationId: string,
-//     //     amount: number
-//     // ) {
-//     //     const transaction =
-//     //         await businessRepository.transferBusinessToIndividual(
-//     //             sourceId,
-//     //             destinationId,
-//     //             amount
-//     //         );
-//     //     return transaction;
-//     // }
-// }
+class BusinessService {
+    async createBusinessAccount(businessDto: ICreateBusinessDto) {
+        const business = await businessRepository.createBusinessAccount(
+            businessDto
+        );
+        return business;
+    }
 
-// const businessService = new BusinessService();
+    async transferBusinessToBusiness(
+        sourceId: string,
+        destinationId: string,
+        amount: number
+    ) {
+        const transaction = await businessRepository.transferBusinessToBusiness(
+            sourceId,
+            destinationId,
+            amount
+        );
+        return transaction;
+    }
 
-// export default businessService;
+    async transferBusinessToIndividual(
+        sourceId: string,
+        destinationId: string,
+        amount: number
+    ) {
+        const transaction =
+            await businessRepository.transferBusinessToIndividual(
+                sourceId,
+                destinationId,
+                amount
+            );
+        return transaction;
+    }
+}
+
+const businessService = new BusinessService();
+
+export default businessService;
