0ad6f33790f083370e33e7431f867f9fb64967ca
Amit Shalev <amits@rapyd.ac>
2022-02-03 16:13:23 +0200

    add: load config.json

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,22 @@
 import fs from "fs";
 import path from "path";
 
-const { cwd } = process;
+interface IConfig {
+    NODE_ENV: string;
+    HOST: string;
+    PORT: string;
+    DB_HOST: string;
+    DB_PORT: string;
+    DB_USERNAME: string;
+    DB_PASSWORD: string;
+    DB_NAME: string;
+    APP_SECRET: string;
+    ACCESS_TOKEN_EXPIRATION: string;
+    REFRESH_TOKEN_EXPIRATION: string;
+}
 
-const config = JSON.parse(
-    fs.readFileSync(path.join(cwd(), "config.json"), "utf-8")
+const config: IConfig = JSON.parse(
+    fs.readFileSync(path.join(process.cwd(), "config.json"), "utf-8")
 );
 
 export default config;
