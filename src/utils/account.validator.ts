5093c9a4e95078d6ad0e6c4159b08a5319150e29
Gideon shachar <gideonshachar@gmail.com>
2022-02-06 19:22:17 +0200

    modified
    == => ===

--- src/utils/account.validator.ts
+++ src/utils/account.validator.ts
@@ -1,19 +1,19 @@
-import { AccountTypes, IAccount } from "../types/accounts.interface.js";
+import { AccountTypes, IAccount, AccountStatuses } from "../types/accounts.interface.js";
 
 class AccountValidator {
 
     isActive = (accounts : IAccount[]) => {
-        const result = accounts.every(account => account.status == 1);
+        const result = accounts.every(account => account.status === AccountStatuses.Active);
         return result;
     };
 
     isTypeOf = (types : AccountTypes[], accounts : IAccount[]) => {
-        const result = accounts.every(account => types.some(type => type == account.type));
+        const result = accounts.every(account => types.some(type => type === account.type));
         return result;
     };
 
     isSameCurrency = (currency : string, accounts : IAccount[]) => {
-        const result = accounts.every(account => account.currency == currency);
+        const result = accounts.every(account => account.currency === currency);
         return result;
     };
 }
