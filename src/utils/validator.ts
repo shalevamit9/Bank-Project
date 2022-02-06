db9dc36277947f1aa37575cc3bf7e7d93782b8d9
Avital Vainer <avital.va0@gmail.com>
2022-02-06 19:11:58 +0200

    feat: added validations at utils/validator.ts

---
+++ src/utils/validator.ts
@@ -0,0 +1,32 @@
+export class Validator {
+    isKeyExist(key_to_find: string, obj: Object): boolean {
+        for (const key in obj) {
+            if (key_to_find === key) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    isPositive(num: number): boolean {
+        return num >= 0;
+    }
+
+    isEmpty(arr: any[]): boolean {
+        return arr.length === 0;
+    }
+
+    isLessThan(limit: number, num: number): boolean {
+        return num < limit;
+    }
+
+    isGreaterThan(threshold: number, num: number): boolean {
+        return num >= threshold;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    isNumeric(value: any): boolean {
+        return typeof value === "number";
+    }
+}
