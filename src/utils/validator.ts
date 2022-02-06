9333e421a7766b617bf156d141bc4e1881a38fe6
Gideon shachar <gideonshachar@gmail.com>
2022-02-06 20:07:40 +0200

    adding validator class
    adding validator functions

---
+++ src/utils/validator.ts
@@ -0,0 +1,47 @@
+import { resourceLimits } from "worker_threads";
+import { IBalanceTransfer,
+    AccountTypes,
+    IAccount,
+    AccountStatuses,
+} from "../types/accounts.interface.js";
+
+class Validator {
+    isPositive = (number: number) => {};
+
+    isEmpty = (array: any[]) => {};
+
+    lowerThan = (validate_number: number, amount: number) => {};
+
+    length = (length_to_validate: number, input: string | number) => {
+        if (typeof input === "string" || typeof input === "number") {
+            return String(input).length === length_to_validate;
+        }
+
+        return false;
+    };
+
+    greaterThan = (validate_numver: number, amount: number) => {};
+
+    isNumeric = (val: any) => {};
+
+    isExist = (accounts: IAccount[], amount: number) => {
+        return accounts.length === amount;
+    };
+
+    // if need account property then use balance
+    hasMinSum = (min: number, amounts: number[]) => {
+        const result = amounts.reduce((sum: number, amount: number): number => {
+            return sum + amount;
+        }, 0);
+        return result >= min;
+    };
+
+    hasMinBalance = (min: number, balanceTransfers: IBalanceTransfer[]) => {
+        const result = balanceTransfers.every(tup => (tup[0] - tup[1]) >= min);
+        return result;
+    };
+}
+
+const validator = new Validator();
+
+export default validator;
