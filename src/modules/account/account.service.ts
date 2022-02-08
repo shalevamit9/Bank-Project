56d05976be2ee355e2a709bf3f6898c89f5cc5c1
Amit Shalev <amits@rapyd.ac>
2022-02-08 15:27:58 +0200

    add getAccountById to service

--- src/modules/account/account.service.ts
+++ src/modules/account/account.service.ts
@@ -2,6 +2,11 @@ import { ICreateAccount } from "./account.interface.js";
 import accountRepository from "./account.repository.js";
 
 class AccountService {
+    async getAccountById(account_id: number) {
+        const account = await accountRepository.getAccountById(account_id);
+        return account;
+    }
+
     async createAccount(create_account: ICreateAccount) {
         const account = await accountRepository.createAccount(create_account);
         return account;
