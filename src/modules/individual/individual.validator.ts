df4592e406be98a7390794ce632b5a611129cd19
Gideon shachar <gideonshachar@gmail.com>
2022-02-09 17:22:51 +0200

    individual validations check
    get and create validation are working well

--- src/modules/individual/individual.validator.ts
+++ src/modules/individual/individual.validator.ts
@@ -1,33 +1,56 @@
 import { Request, Response, RequestHandler, NextFunction } from "express";
 import validator from "../../utils/validator.js";
-import individualService from "./individual.service.js";
-import individualRepository from "./individual.repository.js";
-import { IIndividualAccountDto, IIndividualAccount } from "./individual.interface.js";
+import { IIndividualAccountDto } from "./individual.interface.js";
+import { validationResultsHandler } from "../../utils/validation.utils.js";
+import { IValidationResult } from "../../types/validation.interface.js";
 
 class IndividualValidator {
     createIndividual: RequestHandler = async (req: Request, res: Response, next: NextFunction) => {
-            const individual_dto : IIndividualAccountDto = req.body;
-            validator.required(individual_dto, ["individual_id","first_name","last_name","currency"]);
-            validator.notExist(individual_dto, ["individual_account_id"]);
-            validator.length(7, String(individual_dto.individual_id));
-            validator.isNumeric(individual_dto.individual_id)
-            validator.isGreaterThan(1000000, Number(individual_dto.individual_id));
+        const results: IValidationResult[] = [];
+        const individual_dto : IIndividualAccountDto = req.body;
+        const mandatory_keys = ["individual_id","first_name","last_name","currency"];
 
-            const individual = await individualRepository.getIndividualById(Number(individual_dto.individual_id));
-            if(!individual) throw new Error("individual doesn't exist");
-            
-            validator.isExist([individual],0)
+        results.push({
+            is_valid: validator.required(individual_dto, mandatory_keys),
+            message: `At least one of the following properties are missing [${mandatory_keys.toString()}]`,
+        });
+        results.push({
+            is_valid: validator.notExist(individual_dto, ["individual_account_id"]),
+            message: "individual_account_id can't be exist",
+        });
+        results.push({
+            is_valid: validator.length(7, String(individual_dto.individual_id)),
+            message: "individual_id length is not 7",
+        });
+        results.push({
+            is_valid: validator.isNumeric(individual_dto.individual_id),
+            message: "individual_id is not numeric",
+        });
+        results.push({
+            is_valid: validator.isGreaterThan(1000000, Number(individual_dto.individual_id)),
+            message: "individual_id is not greater than 1000000",
+        });
+        
+        validationResultsHandler(results);
 
-            next();
-        };
+        next();
+    };
 
     getIndividual: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
-            const individual_dto : IIndividualAccountDto = req.body;
-            validator.required(individual_dto, ["individual_account_id"]);
-            validator.isNumeric(individual_dto.individual_id);
+        const results: IValidationResult[] = [];
+        results.push({
+            is_valid: validator.required(req.params, ["id"]),
+            message: `id property is missing `,
+        });
+        results.push({
+            is_valid: validator.isNumeric(req.params.id),
+            message: `individual_id is not numeric`,
+        });
+        
+        validationResultsHandler(results);
 
-            next();
-        };
+        next();
+    };
 }
 
 const individualValidator = new IndividualValidator();
