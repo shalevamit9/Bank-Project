f5f4f214007c3be6bff298fd511a77acbf286e72
Amit Shalev <amits@rapyd.ac>
2022-02-11 18:24:17 +0200

    casting secret to string

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -3,11 +3,10 @@ import { db } from "../db/mysql.connection.js";
 import { RowDataPacket } from "mysql2";
 import crypto from "crypto";
 
-
 interface IMerchant {
-    merchant_id: number,
-    secret_key: string,
-    access_key: string
+    merchant_id: number;
+    secret_key: string;
+    access_key: string;
 }
 
 class Authentication {
@@ -16,9 +15,9 @@ class Authentication {
         res: Response,
         next: NextFunction
     ) => {
-        const secret_key_hash = req.headers["secret_key_hash"];
-        const access_key = req.headers["access_key"]; 
-        
+        const secret_key_hash = req.headers["secret_key_hash"] as string;
+        const access_key = req.headers["access_key"];
+
         const [query_data] = (await db.query(
             `SELECT *
             FROM merchants
@@ -27,17 +26,20 @@ class Authentication {
         )) as RowDataPacket[][];
 
         const merchant = query_data[0] as IMerchant;
-        const hash = crypto.createHash('sha256').update(`$${merchant.secret_key}`).digest('hex');
-        
-        if(hash !== secret_key_hash) {
-            console.log(`our hash: ${hash} \n the hash we got: ${secret_key_hash}`);
+        const hash = crypto
+            .createHash("sha256")
+            .update(`$${merchant.secret_key}`)
+            .digest("hex");
+
+        if (hash !== secret_key_hash) {
+            console.log(
+                `our hash: ${hash} \n the hash we got: ${secret_key_hash}`
+            );
             throw new Error("hashing went wrong");
         }
 
         next();
     };
-
-    
 }
 
 const authentication = new Authentication();
