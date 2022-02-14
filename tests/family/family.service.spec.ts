df5e670935a1c56128232e7b610195dccfaf4586
Avital Vainer <avital.va0@gmail.com>
2022-02-14 22:26:44 +0200

    test: added family service tests

--- tests/family/family.service.spec.ts
+++ tests/family/family.service.spec.ts
@@ -1,7 +1,5 @@
-
 import { expect } from "chai";
 import sinon from "sinon";
-import { ICreateAccount } from "../../src/modules/account/account.interface.js";
 import accountRepository from "../../src/modules/account/account.repository.js";
 import accountService, {
     ITransaction,
@@ -21,8 +19,67 @@ import individualService from "../../src/modules/individual/individual.service.j
 import { AccountTypes, IAccount } from "../../src/types/accounts.interface.js";
 
 describe("Family Service Functions:", () => {
+    const full_family: IFamilyAccountDto = {
+        family_account_id: 23,
+        context: "vacation",
+        account_id: 32,
+        currency: "ILS",
+        balance: 5000,
+        status: 1,
+        type: AccountTypes.Family,
+        owners: [
+            {
+                account_id: 30,
+                balance: 7000,
+                individual_account_id: 2,
+                currency: "ILS",
+                status: 1,
+                type: AccountTypes.Individual,
+                individual_id: 12345,
+                first_name: "amit",
+                last_name: "shalev",
+                email: "amit@gmail.com",
+                address: {
+                    address_id: 1,
+                    city: "Alderetes",
+                    country_code: "AR",
+                    country_name: "147",
+                    postal_code: 4178,
+                    region: "Jeziora Wielkie",
+                    street_name: "Jana",
+                    street_number: 7,
+                },
+            },
+            {
+                account_id: 31,
+                balance: 24000,
+                individual_account_id: 3,
+                currency: "ILS",
+                status: 1,
+                type: AccountTypes.Individual,
+                individual_id: 12645,
+                first_name: "gideon",
+                last_name: "shachar",
+                email: "gidi@gmail.com",
+                address: {
+                    address_id: 1,
+                    city: "Alderetes",
+                    country_code: "AR",
+                    country_name: "147",
+                    postal_code: 4178,
+                    region: "Jeziora Wielkie",
+                    street_name: "Jana",
+                    street_number: 7,
+                },
+            },
+        ],
+    };
     // REDUNDANT!! this functio implements no logic. only calls other functions!
     context("#createFamilyAccount()", () => {
+        afterEach(() => {
+            sinon.restore();
+        });
+
         const create_valid_account1: ICreateFamily = {
             currency: "USD",
             context: "travel",
@@ -51,21 +108,21 @@ describe("Family Service Functions:", () => {
             type: AccountTypes.Family,
         };
 
-        const create_account_stub = sinon.stub(accountService, "createAccount");
-        const create_family_account_stub = sinon.stub(
-            familyRepository,
-            "createFamilyAccount"
-        );
-        const add_family_members_stub = sinon.stub(
-            familyService,
-            "addFamilyMembers"
-        );
+        // const create_account_stub = sinon.stub(accountService, "createAccount");
+        // const create_family_account_stub = sinon.stub(
+        //     familyRepository,
+        //     "createFamilyAccount"
+        // );
+        // const add_family_members_stub = sinon.stub(
+        //     familyService,
+        //     "addFamilyMembers"
+        // );
 
         // REDUNDANT!! this functio implements no logic. only calls other functions!
         it("should create a family object", async () => {
-            create_account_stub.resolves(account_obj);
-            create_family_account_stub.resolves(24);
-            add_family_members_stub.resolves(family_obj);
+            sinon.stub(accountService, "createAccount").resolves(account_obj);
+            sinon.stub(familyRepository, "createFamilyAccount").resolves(24);
+            sinon.stub(familyService, "addFamilyMembers").resolves(family_obj);
             expect(
                 await familyService.createFamilyAccount(create_valid_account1)
             ).to.deep.equal(family_obj);
@@ -73,6 +130,10 @@ describe("Family Service Functions:", () => {
     });
 
     context("#getFamilyById()", () => {
+        afterEach(() => {
+            sinon.restore();
+        });
+
         const short_family: IFamilyAccountDto = {
             family_account_id: 23,
             context: "vacation",
@@ -127,134 +188,142 @@ describe("Family Service Functions:", () => {
             },
         ];
 
-        const full_family: IFamilyAccountDto = {
-            family_account_id: 23,
-            context: "vacation",
-            account_id: 32,
-            currency: "ILS",
-            balance: 5000,
-            status: 1,
-            type: AccountTypes.Family,
-            owners: [
-                {
-                    account_id: 30,
-                    balance: 7000,
-                    individual_account_id: 2,
-                    currency: "ILS",
-                    status: 1,
-                    type: AccountTypes.Individual,
-                    individual_id: 12345,
-                    first_name: "amit",
-                    last_name: "shalev",
-                    email: "amit@gmail.com",
-                    address: {
-                        address_id: 1,
-                        city: "Alderetes",
-                        country_code: "AR",
-                        country_name: "147",
-                        postal_code: 4178,
-                        region: "Jeziora Wielkie",
-                        street_name: "Jana",
-                        street_number: 7,
-                    },
+        const formatted_owners: IIndividualAccountDto[] = [
+            {
+                account_id: 30,
+                balance: 7000,
+                individual_account_id: 2,
+                currency: "ILS",
+                status: 1,
+                type: AccountTypes.Individual,
+                individual_id: 12345,
+                first_name: "amit",
+                last_name: "shalev",
+                email: "amit@gmail.com",
+                address: {
+                    address_id: 1,
+                    city: "Alderetes",
+                    country_code: "AR",
+                    country_name: "147",
+                    postal_code: 4178,
+                    region: "Jeziora Wielkie",
+                    street_name: "Jana",
+                    street_number: 7,
                 },
-                {
-                    account_id: 31,
-                    balance: 24000,
-                    individual_account_id: 3,
-                    currency: "ILS",
-                    status: 1,
-                    type: AccountTypes.Individual,
-                    individual_id: 12645,
-                    first_name: "gideon",
-                    last_name: "shachar",
-                    email: "gidi@gmail.com",
-                    address: {
-                        address_id: 1,
-                        city: "Alderetes",
-                        country_code: "AR",
-                        country_name: "147",
-                        postal_code: 4178,
-                        region: "Jeziora Wielkie",
-                        street_name: "Jana",
-                        street_number: 7,
-                    },
+            },
+            {
+                account_id: 31,
+                balance: 24000,
+                individual_account_id: 3,
+                currency: "ILS",
+                status: 1,
+                type: AccountTypes.Individual,
+                individual_id: 12645,
+                first_name: "gideon",
+                last_name: "shachar",
+                email: "gidi@gmail.com",
+                address: {
+                    address_id: 1,
+                    city: "Alderetes",
+                    country_code: "AR",
+                    country_name: "147",
+                    postal_code: 4178,
+                    region: "Jeziora Wielkie",
+                    street_name: "Jana",
+                    street_number: 7,
                 },
-            ],
-        };
-
-        const get_short_stub = sinon.stub(
-            familyRepository,
-            "getShortFamilyDetails"
-        );
-        const get_full_stub = sinon.stub(
-            familyRepository,
-            "getFullFamilyDetails"
-        );
-        const format_account_callback = sinon.spy(
-            individualService,
-            "formatAccount"
-        );
-
-        it("should be a function", () => {
-            expect(familyService.getFamilyById).to.be.a("function");
-        });
+            },
+        ];
 
         it("should return the family details", async () => {
-            console.log("format_account_callback: ", format_account_callback);
-            console.log(
-                "callback.firstCall: ",
-                format_account_callback.firstCall
-            );
-            console.log(
-                "callback.secondCall.args[0]: ",
-                format_account_callback.secondCall
+            sinon
+                .stub(familyRepository, "getShortFamilyDetails")
+                .resolves(short_family);
+
+            sinon
+                .stub(familyRepository, "getFullFamilyDetails")
+                .resolves(owners);
+
+            const format_account_callback = sinon.stub(
+                individualService,
+                "formatAccount"
             );
 
-            get_short_stub.resolves(short_family);
-            get_full_stub.resolves(owners);
+            format_account_callback.onFirstCall().returns(formatted_owners[0]);
+            format_account_callback.onSecondCall().returns(formatted_owners[1]);
 
             expect(await familyService.getFamilyById(23)).to.deep.equal(
                 full_family
             );
+        });
 
-            expect(format_account_callback.firstCall.args[0]).to.equal(
-                owners[0]
-            );
-            expect(format_account_callback.secondCall.args[0]).to.equal(
-                owners[1]
-            );
+        it("should throw if account with such id doesn't exist", async () => {
+            sinon
+                .stub(familyRepository, "getShortFamilyDetails")
+                .resolves(undefined);
 
-            console.log("format_account_callback: ", format_account_callback);
-            console.log(
-                "callback.firstCall: ",
-                format_account_callback.firstCall
+            try {
+                await familyService.getFamilyById(23);
+            } catch (error) {
+                expect((error as Error).message).to.equal(
+                    "Bad Request - Account doesn't exist"
+                );
+            }
+        });
+    });
+
+    context("#addFamilyMembers()", () => {
+        afterEach(() => {
+            sinon.restore();
+        });
+
+        it("should be called with the correct arguments", async () => {
+            const add_members_stub = sinon.stub(
+                familyRepository,
+                "addMembersToFamily"
             );
-            console.log(
-                "callback.secondCall.args[0]: ",
-                format_account_callback.secondCall
+            sinon.stub(familyService, "getFamilyById").resolves(full_family);
+            await familyService.addFamilyMembers(23, [
+                [1, 1000],
+                [2, 1000],
+            ]);
+            sinon.assert.calledWith(
+                add_members_stub,
+                23,
+                [
+                    [1, 1000],
+                    [2, 1000],
+                ],
+                2000
             );
         });
     });
 
-    context("addFamilyMembers", () => {
-        it("should be a function", () => {
-            expect(familyService.addFamilyMembers).to.be.a("function");
+    context("#removeFamilyMembers()", () => {
+        afterEach(() => {
+            sinon.restore();
         });
-    });
 
-    context("removeFamilyMembers", () => {
-        it("should be a function", () => {
-            expect(familyService.removeFamilyMembers).to.be.a("function");
+        it("should be called with the correct arguments", async () => {
+            const remove_members_stub = sinon.stub(
+                familyRepository,
+                "removeMembersFromFamily"
+            );
+            sinon.stub(familyService, "getFamilyById").resolves(full_family);
+            await familyService.removeFamilyMembers(23, [
+                [1, 1000],
+                [2, 1000],
+            ]);
+            sinon.assert.calledWith(remove_members_stub, 23, [
+                [1, 1000],
+                [2, 1000],
+            ]);
         });
     });
 
     context("#closeFamilyAccount()", () => {
-        // DONE
-        after(() => {
+        afterEach(() => {
             sinon.restore();
-            //     get_by_id_stub.restore();
-            //     close_account_stub.restore();
         });
 
         const valid_family: IFamilyAccountDto = {
@@ -290,47 +359,44 @@ describe("Family Service Functions:", () => {
             owners: [],
         };
 
-        const get_by_id_stub = sinon.stub(familyService, "getFamilyById");
-
-        const close_account_stub = sinon.stub(
-            familyRepository,
-            "closeFamilyAccount"
-        );
-
         it("should throw an error when there are still owners left in the family", async () => {
-            get_by_id_stub.resolves(invalid_owners_family);
+            sinon
+                .stub(familyService, "getFamilyById")
+                .resolves(invalid_owners_family);
 
             try {
                 await familyService.closeFamilyAccount(23);
-            } catch (err: any) {
-                expect(err.message).to.equal(
+            } catch (err) {
+                expect((err as Error).message).to.equal(
                     "Bad Request - can't close family account because there are still owners left"
                 );
             }
         });
 
         it("should throw an error when the family account status is already 'inactive'", async () => {
-            get_by_id_stub.resolves(invalid_status_family);
+            sinon
+                .stub(familyService, "getFamilyById")
+                .resolves(invalid_status_family);
 
             try {
                 await familyService.closeFamilyAccount(23);
-            } catch (err: any) {
-                expect(err.message).to.equal(
+            } catch (err) {
+                expect((err as Error).message).to.equal(
                     "Bad Request - account is already closed"
                 );
             }
         });
 
         it("should change the family status to 'inactive'", async () => {
-            get_by_id_stub.resolves(valid_family);
-            close_account_stub.resolves(true);
+            sinon.stub(familyService, "getFamilyById").resolves(valid_family);
+            sinon.stub(familyRepository, "closeFamilyAccount").resolves(true);
 
             expect(await familyService.closeFamilyAccount(23)).to.be.true;
         });
     });
 
     context("#transferToBusiness()", () => {
-        after(() => {
+        afterEach(() => {
             sinon.restore();
         });
 
@@ -377,27 +443,41 @@ describe("Family Service Functions:", () => {
             },
         };
 
-        const get_family_stub = sinon.stub(familyRepository, "getFamilyById");
-        const get_business_stub = sinon.stub(
-            businessRepository,
-            "getBusinessById"
-        );
-        const account_repo_transfer_stub = sinon
-            .stub(accountRepository, "transfer");
-            // .resolves(true);
-        const transfer_spy = sinon.spy(accountService, "transfer");
+        // const transfer_spy = sinon.spy(accountService, "transfer");
 
         it("should throw an error when maximal transfer amount limit is exceeded", async () => {
-            // expect(await familyService.transferToBusiness(x, y, z)).to.deep.equal("Bad Request - Cannot perform transfer - Invalid amount");
+            sinon
+                .stub(familyRepository, "getFamilyById")
+                .resolves(source_account);
+            sinon
+                .stub(businessRepository, "getBusinessById")
+                .resolves(destination_account);
+
+            try {
+                await familyService.transferToBusiness(22, 4, 6500);
+            } catch (error) {
+                expect((error as Error).message).to.equal(
+                    "Bad Request - Cannot perform transfer - Invalid amount"
+                );
+            }
         });
 
         it("should return the updated accounts details after the transfer", async () => {
-            get_family_stub.resolves(source_account);
-            get_business_stub.resolves(destination_account);
-            account_repo_transfer_stub.resolves(true);
-            expect(
-                await familyService.transferToBusiness(22, 4, 2000)
-            ).to.deep.equal(transfer_response);
+            sinon
+                .stub(familyRepository, "getFamilyById")
+                .resolves(source_account);
+            sinon
+                .stub(businessRepository, "getBusinessById")
+                .resolves(destination_account);
+
+            sinon.stub(accountRepository, "transfer").resolves(true);
+
+            const family_transfer = await familyService.transferToBusiness(
+                22,
+                4,
+                2000
+            );
+            expect(family_transfer).to.deep.equal(transfer_response);
         });
     });
 });
